8125a55897df348a086aa5580edd056dd6e66a8d
platoibi <ibrahimtaghizade@gmail.com>
2023-10-27 23:31:17 +0400

    implemented the connector

--- sender.py
+++ sender.py
@@ -1,6 +1,13 @@
+import psycopg2
+
 def connectToDB(ip :str):
-    pass
+    conn = psycopg2.connect(
+        database="hw1", user='dist_user', password='dist_pass_123', host=ip, port= '5432'
+    )
+    cur = conn.cursor()
+    return cur
 
 servers = open("databases.txt").readlines()
+serverCursors = []
 for server in servers:
-    connectToDB(server)
+    serverCursors.append(connectToDB(server))
